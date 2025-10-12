bd75880f9863e4a9ec0317acfc739fa05a8c6ab9
aelhadda <eaub@proton.me>
2025-10-12 03:09:32 +0100

    working on user model

--- backend/src/main/java/com/libre/dto/RegisterRequest.java
+++ backend/src/main/java/com/libre/dto/RegisterRequest.java
@@ -8,9 +8,23 @@ public class RegisterRequest {
     private Integer age;
     private String password;
 
-    // getters/setters
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String name) { this.firstName = name; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String name) { this.lastName = name; }
+
+    public String getFullName() { return this.firstName + this.lastName; }
+
+    public Integer getAge() {return this.age;}
+    public void setAge(Integer age) {this.age = age;}
+    
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 }
---
+++ backend/src/main/java/com/libre/model/User.java
@@ -0,0 +1,13 @@
+package com.libre.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    
+}

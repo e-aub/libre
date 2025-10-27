1ed4c8b87990934d67fae870a123593d7a0820fc
e-aub <eaub@proton.me>
2025-10-27 03:08:50 +0100

    adding routing...

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,18 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/header/header.component';
-import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { AuthModalComponent } from './auth/auth-modal/auth-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, WelcomeComponent, AuthModalComponent],
+  imports: [RouterOutlet],
   template : `
-    <app-header/>
-    <app-welcome/>
-    <app-auth-modal />
+      <router-outlet></router-outlet>
   `,
 })
 export class App {
-  protected readonly title = signal('frontend');
 }
--- frontend/src/app/pages/welcome/welcome.component.ts
+++ frontend/src/app/pages/welcome/welcome.component.ts
@@ -3,13 +3,23 @@ import { AuthModalComponent } from '../../auth/auth-modal/auth-modal.component';
 import { ModalService } from '../../core/modal.service';
 
 @Component({
-  imports: [],
+  imports: [AuthModalComponent],
   selector: 'app-welcome',
   template: `
+    <header class="header-container">
+                <div class="header-content">
+                <a href="/" class="logo-link">
+                    <img src="logo.png" alt="Libre Logo" class="logo-img">
+                </a>
+                
+                <div class="actions">
+                    <button class="btn secondary-btn" (click)="authModalService.openModal('register')">Join us</button> 
+                    <button class="btn primary-btn" (click)="authModalService.openModal('login')">Login</button>
+                </div>
+                </div>
+    </header>
     <section class="welcome-section">
       <div class="welcome-content-wrapper">
-          <div>{{count}}</div>
-          
           <div class="content-area">
             <p class="h1-title headline-line">Share Thoughts</p>
             <p class="h1-title headline-line">Shape Minds</p>
@@ -26,15 +36,13 @@ import { ModalService } from '../../core/modal.service';
       </div>
       
     </section>
+    <app-auth-modal />
   `,
   styleUrl: './welcome.component.css',
 })
 export class WelcomeComponent {
     public authModalService = inject(ModalService);
-    count = 1;
 
-    increment(){
-      this.count += 1;
-    }
+  
     
 }
